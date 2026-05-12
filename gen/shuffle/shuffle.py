7df4bd4d94726ab4ddadddb299d1deb1b463d831
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2026-05-12 17:26:58 +0530

    high mid low 3 lang init

--- gen/shuffle/shuffle.py
+++ gen/shuffle/shuffle.py
@@ -5,9 +5,9 @@ from pathlib import Path
 
 # Hardcoded directory containing the source .txt files.
 SOURCE_DIR = Path(
-	r"C:\Users\wmdha\Documents\UoM BSc IT\Lessons\L4S7\Reserach - Group project\Code\gen data 2\docs\categories\1_high"
+	r"C:\Users\wmdha\Documents\UoM BSc IT\Lessons\L4S7\Reserach - Group project\Code\gen data 2\docs\categories\2_mid"
 )
-TARGET_FILE_NAME = "A.txt"
+TARGET_FILE_NAME = "E.txt"
 OUTPUT_FILE = Path(__file__).resolve().parent / "shuffled.txt"
 
 
